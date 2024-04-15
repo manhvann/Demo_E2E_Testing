f44c6292cd53abb31d6903354d9237e8dbe61703
manhvann <manhvann193@gmail.com>
2024-04-15 13:41:01 +0700

    test

---
+++ Testing/src/test/java/seleniumE2E/AddToCart_Checkout.java
@@ -0,0 +1,5 @@
+package seleniumE2E;
+
+public class AddToCart_Checkout extends BaseTest{
+    
+}
---
+++ Testing/src/test/java/seleniumE2E/BaseTest.java
@@ -0,0 +1,28 @@
+package seleniumE2E;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://ecommerce-playground.lambdatest.io/");
+
+
+    }
+
+
+    @AfterMethod
+    public void tearDown(){
+//        driver.quit();
+    }
+}
